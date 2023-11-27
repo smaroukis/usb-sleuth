182ee3132cd74a4f4f3ebb4832f8736c338b4eaf
Spencer <smaroukis@gmail.com>
2023-11-27 12:09:36 +0900

    code - gnd and vbus testing

---
+++ src/usb-tester/usb-tester.ino
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+// Set LED_BUILTIN if it is not defined by Arduino framework
+#ifndef LED_BUILTIN
+    #define LED_BUILTIN 13
+#endif
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+   // wait for a second
+  delay(1000);
+}
